7323b316a9d6fffe83ebc49c203869fa8b716a7d
scrappy1987 <jagordon87@msn.com>
2018-07-16 12:35:29 +0100

    first commit

---
+++ src/main/webapp/app/feature/account/account-controller.js
@@ -0,0 +1,32 @@
+"use strict";
+
+(function() {
+
+    var AccountController =  function(accountService, $log) {
+        
+    	var vm = this;
+        
+        vm.isHidden = false;
+        
+        vm.hideTable = function()
+        {
+        	vm.isHidden = !vm.isHidden
+        };
+        
+        function init() {
+        	accountService.getAccounts().then(function (results) {
+           	vm.accounts = results;
+           	$log.log("In the account controller the value of the result promise is ");
+        	$log.log(JSON.stringify(vm.accounts));
+            }, function (error) {
+                vm.error = true;
+                vm.errorMessage = error;
+            });
+       }
+       
+       init();
+            
+    };
+
+    angular.module('accountApp').controller('accountController', ['accountService','$log', AccountController]);
+}());
